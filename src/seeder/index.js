0a4c88b68c166a14a1cd9d479a1dabcdad509d5c
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-07-26 16:24:04 +0200

    feat: seed products images

---
+++ src/seeder/index.js
@@ -0,0 +1,26 @@
+const dataArray = require("./products.json");
+const fs = require("fs");
+const path = require("path");
+const { putObject } = require("../s3-client");
+
+function seed() {
+  try {
+    dataArray.forEach((productData) => {
+      const { fileName } = productData;
+      const filePath = path.resolve("src/seeder/images/", fileName); // Replace with the actual file path
+      fs.readFile(filePath, "utf8", async (err, data) => {
+        if (err) {
+          console.error("Error reading the file:", err);
+        } else {
+          const key = `products-image/${fileName}`;
+          await putObject(filePath, key);
+          console.log("uploaded: ", key);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+seed();
